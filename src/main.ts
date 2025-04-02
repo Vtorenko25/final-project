b4deab4ff9b631e0163d9acf5d84da1e3da2a4d5
Vtorenko25 <oleg.vtorenko@gmail.com>
2025-04-02 20:42:06 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import { config } from "./configs/config";
+import * as mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 
 app.listen(config.port, async () => {
-  // await mongoose.connect(config.mongoUrl);
+  await mongoose.connect(config.mongoUrl);
   console.log(`Server has been started on port ${config.port}`);
 });
